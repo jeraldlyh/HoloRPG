081d8c37a0278e4b43532bbdad4a3d3137f96397
Jerald <jeraldssw@gmail.com>
2020-12-13 15:17:12 +0800

    Template for discord main file

--- discord/bot.py
+++ discord/bot.py
@@ -1,1 +1,21 @@
 import discord
+
+from discord.config import BOT_TOKEN
+
+bot = commands.Bot(command_prefix='.')
+bot.remove_command('help')
+
+extensions = [
+
+    ]
+
+
+if __name__ == "__main__":
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+            print('Loaded {0}'.format(extension))
+        except Exception as e:
+            raise Exception
+
+bot.run(f'{BOT_TOKEN}')
