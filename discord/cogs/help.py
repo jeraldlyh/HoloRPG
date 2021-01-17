86bab72eec971dae7ce7f84fa72207feef715c1f
Jerald <jeraldssw@gmail.com>
2021-01-17 18:19:40 +0800

    Standardised double quotes according to Python's convention

--- discord/cogs/help.py
+++ discord/cogs/help.py
@@ -6,10 +6,10 @@ class Help(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
     
-    @commands.command(description='Help commands', hidden=True)
-    async def help(self, ctx, cog='1'):
+    @commands.command(description="Help commands", hidden=True)
+    async def help(self, ctx, cog="1"):
         helpEmbed = discord.Embed(
-            title='Command List'
+            title="Command List"
         )
         helpEmbed.set_thumbnail(url=ctx.author.avatar_url)
 
@@ -18,13 +18,13 @@ class Help(commands.Cog):
         for cog in cogs:
             currentcog = self.bot.get_cog(cog)
             commands = currentcog.get_commands()
-            commandList = ''
+            commandList = ""
             
             for command in commands:
                 if command.hidden != True:
                     helpEmbed.add_field(
                         name=command.name, 
-                        value=f'Description: {command.description}',
+                        value=f"Description: {command.description}",
                         inline=False
                         )
 
