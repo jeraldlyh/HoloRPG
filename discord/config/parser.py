230f865c153c89551f9afe308a9bfc91b326d200
Jerald <jeraldssw@gmail.com>
2021-01-17 18:20:29 +0800

    Standardised double quotes according to Python's convention

--- discord/config/parser.py
+++ discord/config/parser.py
@@ -4,9 +4,9 @@ import codecs
 
 class Parser:
     def __init__(self):
-        self.configFile = r'{0}/config/config.ini'.format(os.getcwd())
+        self.configFile = r"{0}/config/config.ini".format(os.getcwd())
         config = configparser.ConfigParser()
-        config.read_file(codecs.open(self.configFile, 'r', 'utf-8-sig'))
+        config.read_file(codecs.open(self.configFile, "r", "utf-8-sig"))
         
-        self.discordKey = config['Discord']['Key']
-        self.dbPath = config['DB']['Path']
+        self.discordKey = config["Discord"]["Key"]
+        self.dbPath = config["DB"]["Path"]
--- discord/utils/checks.py
+++ discord/utils/checks.py
@@ -8,39 +8,39 @@ class NotChosenClass(commands.CheckFailure):
     pass
 
 def has_registered():
-    '''Checks if user has been registered in database'''
+    """Checks if user has been registered in database"""
 
     def predicate(ctx):
-        database = sqlite3.connect('users.db')
+        database = sqlite3.connect("users.db")
         cursor = database.cursor()
-        cursor.execute(f'SELECT user_id FROM profile WHERE user_id = {ctx.author.id}')
+        cursor.execute(f"SELECT user_id FROM profile WHERE user_id = {ctx.author.id}")
         result = cursor.fetchone()
         if result is None:
             # <user> have not registered in the game yet.
-            raise NotRegistered('You have **not registered** in the game yet')
+            raise NotRegistered("You have **not registered** in the game yet")
         return True
     return commands.check(predicate)
 
 def has_chosen_class():
-    '''Checks if user has chosen a class'''
+    """Checks if user has chosen a class"""
 
     def predicate(ctx):
-        database = sqlite3.connect('users.db')
+        database = sqlite3.connect("users.db")
         cursor = database.cursor()
-        cursor.execute(f'SELECT user_id FROM classes WHERE user_id = {ctx.author.id}')
+        cursor.execute(f"SELECT user_id FROM classes WHERE user_id = {ctx.author.id}")
         result = cursor.fetchone()
         if result is None:
             # <user> have not chosen a class yet.
-            raise NotRegistered('You have **not chosen** a class yet')
+            raise NotRegistered("You have **not chosen** a class yet")
         return True
     return commands.check(predicate)
 
 def user_has_registered(userID):
-    '''Checks if a particular user has been registered in database'''
+    """Checks if a particular user has been registered in database"""
     
-    database = sqlite3.connect('users.db')
+    database = sqlite3.connect("users.db")
     cursor = database.cursor()
-    cursor.execute(f'SELECT user_id FROM profile WHERE user_id = {userID}')
+    cursor.execute(f"SELECT user_id FROM profile WHERE user_id = {userID}")
     result = cursor.fetchone()
     if result is None:
         return False
