8c0a074eecf4165cd42fba4f0fa4c7907ed2ae9f
Jerald <jeraldssw@gmail.com>
2021-05-26 19:20:41 +0800

    Added axios interceptors to refresh JWT tokens

--- website/controller/frontend/src/axios.js
+++ website/controller/frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = "http://127.0.0.1:8000/"
+const baseURL = "http://127.0.0.1:8000"
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
@@ -13,5 +13,60 @@ const axiosInstance = axios.create({
         "accept": "application/json",
     }
 })
+
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    async function (error) {
+        const originalRequest = error.config
+
+        if (  // Prevent it from going into infite loop
+			error.response.status === 401 &&
+			originalRequest.url === baseURL + '/api/token/refresh/'
+		) {
+            console.log("Refresh token is no longer valid")
+            return Promise.reject(error)
+        }
+
+        if (
+            error.response.status === 401 &&
+            error.response.statusText === "Unauthorized"
+        ) {
+            const refreshToken = localStorage.getItem("refresh_token")
+
+            if (refreshToken) {
+                const tokenParts = JSON.parse(atob(refreshToken.split(".")[1]))         // Retrieves the time of the token
+                
+                const now = Math.ceil(Date.now() / 1000)
+				console.log(tokenParts.exp)
+
+                if (tokenParts.exp > now) {
+                    axiosInstance.post("/api/token/refresh/", { refresh_token: refreshToken })
+                        .then((response) => {
+                            if (response.status === 201) {
+                                localStorage.setItem("access_token", response.data.access)
+                                localStorage.setItem("refresh_token", response.data.refresh)
+
+                                axiosInstance.defaults.headers["Authorization"] = "Bearer" + response.data.access
+                                originalRequest.defaults.headers["Authorization"] = "Bearer" + response.data.access
+                                console.log("Refreshed token")
+
+                                return axiosInstance(originalRequest)
+                            }
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+                } else {
+                    console.log("Refresh token is expired", tokenParts.exp, now)
+                }
+            } else {
+                console.log("Refresh token not available")
+            }
+        }
+        return Promise.reject(error)
+    }
+)
 
 export default axiosInstance
