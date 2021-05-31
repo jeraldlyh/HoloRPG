803931cc22b1185f6fb83cb268894774710ef944
Jerald <jeraldssw@gmail.com>
2021-05-31 17:35:11 +0800

    Granted permission for register endpoint

--- website/accounts/views.py
+++ website/accounts/views.py
@@ -2,11 +2,14 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 from .serializers import LoginSerializer, RegisterSerializer
 
 
 
 class RegisterAPIView(APIView):
+    permission_classes = [AllowAny, ]
+
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
