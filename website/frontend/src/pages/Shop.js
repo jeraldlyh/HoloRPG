c7c2fa7c0982efbc0afa186a29c747d1fb55f95f
Jerald <jeraldssw@gmail.com>
2021-06-02 21:14:07 +0800

    Mapped purchase button to frontend and created button state

--- website/frontend/src/pages/Shop.js
+++ website/frontend/src/pages/Shop.js
@@ -4,22 +4,24 @@ import { connect } from "react-redux"
 import { FiPlusCircle, FiMinusCircle } from "react-icons/fi"
 import Layout from "../components/layout/Layout"
 import PageHeader from "../components/layout/PageHeader"
+import profile from "../reducers/profile"
 
 
 function Shop(props) {
     const { user } = props.auth
+    const { currency } = props.profile
     const [entities, setEntities] = useState([])
     const [playerEntities, setPlayerEntities] = useState([])
 
     useEffect(() => {
         axiosInstance.get("/api/entity")
             .then((response) => {
-                const updatedData = []
+                const updatedData = []      // Manually add quantity attribute in entity object
+
                 response.data.forEach(entity => {
                     entity["quantity"] = 0
                     updatedData.push(entity)
                 })
-                console.log(updatedData)
                 setEntities(updatedData)
             })
     }, [])
@@ -28,7 +30,6 @@ function Shop(props) {
         axiosInstance.get(`/api/userentity/${user}`)
             .then(response => {
                 setPlayerEntities(response.data)
-                console.log(response.data)
             })
     }, [])
 
@@ -40,16 +41,49 @@ function Shop(props) {
         }
     }
 
+    const purchaseEntity = (entityName, quantity) => {
+        const body = {
+            user: user,
+            entity: entityName,
+            quantity: quantity
+        }
+        axiosInstance.post("/api/userentity/", body)
+            .then(response => {
+                setPlayerEntities(response.data)
+            })
+            .then(() => {
+                document.getElementById(entityName).value = 0       // Resets input field after submission
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     const increaseQuantity = (index) => {
         const oldEntities = [...entities]
         oldEntities[index].quantity += 1
+        console.log(oldEntities)
         setEntities(oldEntities)
     }
 
     const decreaseQuantity = (index) => {
         const oldEntities = [...entities]
         oldEntities[index].quantity === 0 ? oldEntities[index].quantity : oldEntities[index].quantity -= 1
+        console.log(oldEntities)
         setEntities(oldEntities)
+    }
+
+    const onManualEdit = (e, index) => {
+        const oldEntities = [...entities]
+        oldEntities[index].quantity = parseInt(e.target.value) >= 0 ? parseInt(e.target.value) : oldEntities[index].quantity
+        setEntities(oldEntities)
+    }
+
+    const hasInsufficientCurrency = (cost, quantity) => {
+        console.log(cost, quantity)
+        console.log(currency)
+        console.log((cost * quantity) > currency)
+        return (cost * quantity) > currency
     }
 
     return (
@@ -77,13 +111,13 @@ function Shop(props) {
                                     </div>
                                 </div>
                                 <div className="flex flex-col w-1/5 gap-y-2">
-                                    <input className="text-center" placeholder={entity.quantity}></input>
+                                    <input className="text-center border-2 border-white bg-black" id={entity.name} value={entity.quantity} placeholder={entity.quantity} onChange={e => onManualEdit(e, index)}  />
                                     <div className="flex gap-x-7 justify-center">
                                         <button className="focus:outline-none hover:outline-none hover:text-red-500" onClick={() => decreaseQuantity(index)}><FiMinusCircle  size={28}/></button>
                                         <button className="focus:outline-none hover:outline-none hover:text-custom-green" onClick={() => increaseQuantity(index)}><FiPlusCircle size={28}/></button>
                                     </div>
                                     <span className="text-center">Currently owned: {getEntityOwned(entity.name)}</span>
-                                    <button className="border-2 rounded-full border-white focus:outline-none">Buy</button>
+                                    <button className="border-2 rounded-full border-white focus:outline-none disabled:bg-red-500 " onClick={() => purchaseEntity(entity.name, entity.quantity)} disabled={hasInsufficientCurrency(entity.cost, entity.quantity)}>Buy</button>
                                 </div>
                             </div>
                         )
@@ -96,7 +130,8 @@ function Shop(props) {
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth
+    auth: state.auth,
+    profile: state.profile.profile
 }) 
 
 export default connect(mapStateToProps)(Shop)
