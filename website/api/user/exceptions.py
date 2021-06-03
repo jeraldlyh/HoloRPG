da8e6d4b41cdc30ff829db0a9ed77ad207e14e37
Jerald <jeraldssw@gmail.com>
2021-06-03 14:48:08 +0800

    Created custom exceptions

---
+++ website/api/user/exceptions.py
@@ -0,0 +1,14 @@
+class BountyClaimed(Exception):
+    pass
+
+class InsufficientHealth(Exception):
+    pass
+
+class InsufficientCurrency(Exception):
+    pass
+
+class BountyOnSameUser(Exception):
+    pass
+
+class BountyExist(Exception):
+    pass
