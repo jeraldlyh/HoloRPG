86b9cb12a16e299071ed3645a9608360674fe0d5
Jerald <jeraldssw@gmail.com>
2021-07-10 22:02:19 +0800

    WIP: Banner

---
+++ website/frontend/src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Banner() {
+    return (
+        <div className="w-screen h-24">
+            <div className="p-3">
+                <p className="text-white text-md">Holo</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Banner
