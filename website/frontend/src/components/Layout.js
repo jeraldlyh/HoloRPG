a45bf1ac49706917b4c7a94fdd9faceb1092d762
Jerald <jeraldssw@gmail.com>
2021-07-10 22:08:58 +0800

    Squashed commit of the following:
    commit 13aa25d49530fab99d015402e13126188c28ede2
    Merge: b3d57f8 621e410
    Author: Jerald <jeraldssw@gmail.com>
    Date:   Sat Jul 10 22:04:27 2021 +0800
    
        Merge branch 'feature-layout' of https://github.com/jeraldlyh/HoloRPG into feature-layout
    
    commit b3d57f8cee08cd99d2c2a85b03ab36af313678c1
    Author: Jerald <jeraldssw@gmail.com>
    Date:   Sat Jul 10 22:02:19 2021 +0800
    
        WIP: Banner
    
    commit 92bc6707e326d1e36f9585789e4dbe8c87fb9793
    Author: sherry <sherrychanxl@gmail.com>
    Date:   Sat Jul 10 21:21:20 2021 +0800
    
        WIP: Profile Bar
    
    commit cab9bb6d5519ddadfc78fbf7367de5dab4600dd1
    Author: Jerald <jeraldssw@gmail.com>
    Date:   Sat Jul 10 22:02:19 2021 +0800
    
        WIP: Banner
    
    commit 43d18007a51967e7737797fd782de49464a563eb
    Author: sherry <sherrychanxl@gmail.com>
    Date:   Sat Jul 10 21:21:20 2021 +0800
    
        WIP: Profile Bar
    
    commit 621e41062d130d4a7935f9da81354e599ca66d4c
    Merge: 53674ad 86b9cb1
    Author: Jerald <jeraldssw@gmail.com>
    Date:   Sat Jul 10 22:03:25 2021 +0800
    
        Merge branch 'feature-layout' of https://github.com/jeraldlyh/HoloRPG into feature-layout
    
    commit 53674adca0d35d096b8d242dad9204f6337800bf
    Author: Jerald <jeraldssw@gmail.com>
    Date:   Sat Jul 10 22:02:19 2021 +0800
    
        WIP: Banner
    
    commit b7b5d29cb1adfe269cda09759fd75c1357c4096e
    Author: sherry <sherrychanxl@gmail.com>
    Date:   Sat Jul 10 21:21:20 2021 +0800
    
        WIP: Profile Bar
    
    commit 86b9cb12a16e299071ed3645a9608360674fe0d5
    Author: Jerald <jeraldssw@gmail.com>
    Date:   Sat Jul 10 22:02:19 2021 +0800
    
        WIP: Banner
    
    commit fd3b524fb69c4ef8e408a9d9782fd1d5f1bad291
    Author: sherry <sherrychanxl@gmail.com>
    Date:   Sat Jul 10 21:21:20 2021 +0800
    
        WIP: Profile Bar

--- website/frontend/src/components/Layout.js
+++ website/frontend/src/components/Layout.js
@@ -3,20 +3,22 @@ import Footer from "./Footer"
 import Header from "./Header"
 import NavBar from "./NavBar"
 import Error from "./Error"
+import ProfileBar from "./ProfileBar"
 import FriendBar from "./FriendBar"
 import ProfileCard from "./ProfileCard"
+import Banner from "./Banner"
 
 
 function Layout(props) {
     return (
         <div className="flex flex-row bg-custom-bg-main h-screen">
             <NavBar />
-            <div className="">
-
+            <div className="flex flex-col">
+                <Banner />
+                {props.children}
+                <Error />
             </div>
-            <Error />
-            {props.children}
-
+            <ProfileBar />
             <div className="w-14 bg-black h-min-screen"></div>
         </div>
     )
