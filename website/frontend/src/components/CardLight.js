48f61ec6577c5792436bf5d1d9655eb9227adc9e
Jerald <37609749+jeraldlyh@users.noreply.github.com>
2021-07-13 18:08:38 +0800

    Create light card component

---
+++ website/frontend/src/components/CardLight.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+function CardLight(props) {
+    return (
+        <div className="rounded-lg h-full w-7/12 p-4 bg-custom-card-light border border-custom-color-grey text-white">
+            {
+                props.header
+                    ? <div className="flex flex-col items-center max-w-max">
+                        <div className="flex justify-around items-center">
+                            &nbsp;{props.icon}
+                            <p className="ml-3 font-bold text-lg">{props.title}&nbsp;</p>
+                        </div>
+                        <hr className="border border-custom-misc-accent w-full mt-1" />
+                    </div>
+                    : null
+            }
+            <div className="flex flex-col mt-3">
+                {props.children}
+            </div>
+        </div>
+    )
+}
+
+export default CardLight
--- website/frontend/src/components/home/Leaderboard.js
+++ website/frontend/src/components/home/Leaderboard.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState, Fragment } from "react"
+import React, { useEffect, useState } from "react"
 import _ from "lodash"
 import { connect } from "react-redux"
 import { IoPodiumOutline } from "react-icons/io5"
 import { getAllProfile } from "../../store/actions/Profile"
 import NumberFormat from "react-number-format"
+import CardLight from "../CardLight"
 
 function Leaderboard(props) {
     const [userProfiles, setUserProfiles] = useState([])
@@ -29,47 +30,38 @@ function Leaderboard(props) {
     }, [])
 
     return (
-        <div className="rounded-lg h-full w-7/12 p-4 bg-custom-card-light border border-custom-color-grey text-white">
-            <div className="flex flex-col items-center max-w-max">
-                <div className="flex justify-around items-center">
-                    &nbsp;<IoPodiumOutline />
-                    <p className="ml-3 font-bold text-lg">Leaderboards&nbsp;</p>
-                </div>
-                <hr className="border border-custom-misc-accent w-full mt-1" />
+        <CardLight header={true} title="Leaderboards" icon={<IoPodiumOutline />}>
+            <div className="flex justify-around mb-3">
+                <span className="w-14 text-center font-semibold">RANK</span>
+                <span className="w-32 text-center font-semibold">PLAYER</span>
+                <span className="w-14 text-center font-semibold">CLASS</span>
+                <span className="w-20 text-center font-semibold">AGE</span>
+                <span className="w-24 text-center font-semibold">NET WORTH</span>
             </div>
-            <div className="flex flex-col mt-3">
-                <div className="flex justify-around mb-3">
-                    <span className="w-14 text-center font-semibold">RANK</span>
-                    <span className="w-32 text-center font-semibold">PLAYER</span>
-                    <span className="w-14 text-center font-semibold">CLASS</span>
-                    <span className="w-20 text-center font-semibold">AGE</span>
-                    <span className="w-24 text-center font-semibold">NET WORTH</span>
-                </div>
-                {
-                    userProfiles
-                        ? userProfiles.map((profile, index) => {
-                            return (
-                                <div key={index} className="flex justify-around items-center text-sm">
-                                    <p className="w-14 text-center">#{index + 1}</p>
-                                    <div className="flex items-center w-32">
-                                        <div className="w-8 h-8 bg-white"></div>
-                                        <p className="flex flex-col ml-3">
-                                            <span>{profile.user}</span>
-                                            <span>Lv. {profile.level}</span>
-                                        </p>
-                                    </div>
-                                    <p className="w-14 text-center">{profile.character}</p>
-                                    <p className="w-20 text-center">{profile.account_age} days</p>
-                                    <p className="w-24 text-center text-custom-stats-net_worth">
-                                        <NumberFormat value={profile.net_worth} displayType={"text"} thousandSeparator={true} prefix={"$"} />
+            {
+                userProfiles
+                    ? userProfiles.map((profile, index) => {
+                        return (
+                            <div key={index} className="flex justify-around items-center text-sm">
+                                <p className="w-14 text-center">#{index + 1}</p>
+                                <div className="flex items-center w-32">
+                                    <div className="w-8 h-8 bg-white"></div>
+                                    <p className="flex flex-col ml-3">
+                                        <span>{profile.user}</span>
+                                        <span>Lv. {profile.level}</span>
                                     </p>
                                 </div>
-                            )
-                        })
-                        : null
-                }
-            </div>
-        </div>
+                                <p className="w-14 text-center">{profile.character}</p>
+                                <p className="w-20 text-center">{profile.account_age} days</p>
+                                <p className="w-24 text-center text-custom-stats-net_worth">
+                                    <NumberFormat value={profile.net_worth} displayType={"text"} thousandSeparator={true} prefix={"$"} />
+                                </p>
+                            </div>
+                        )
+                    })
+                    : null
+            }
+        </CardLight>
     )
 }
 
